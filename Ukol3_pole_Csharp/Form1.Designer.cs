f638209266fd751222a9b19c2f1ef5f935109e11
Filip Iran <iranf.03@spst.cz>
2022-01-04 20:02:43 +0100

    v1.0

--- Ukol3_pole_Csharp/Form1.Designer.cs
+++ Ukol3_pole_Csharp/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.vstup = new System.Windows.Forms.TextBox();
+            this.vystup = new System.Windows.Forms.TextBox();
+            this.zmena = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vstup
+            // 
+            this.vstup.Location = new System.Drawing.Point(13, 13);
+            this.vstup.Name = "vstup";
+            this.vstup.Size = new System.Drawing.Size(100, 20);
+            this.vstup.TabIndex = 0;
+            // 
+            // vystup
+            // 
+            this.vystup.Location = new System.Drawing.Point(13, 39);
+            this.vystup.Name = "vystup";
+            this.vystup.ReadOnly = true;
+            this.vystup.Size = new System.Drawing.Size(100, 20);
+            this.vystup.TabIndex = 1;
+            // 
+            // zmena
+            // 
+            this.zmena.Location = new System.Drawing.Point(119, 24);
+            this.zmena.Name = "zmena";
+            this.zmena.Size = new System.Drawing.Size(75, 23);
+            this.zmena.TabIndex = 2;
+            this.zmena.Text = "Změna";
+            this.zmena.UseVisualStyleBackColor = true;
+            this.zmena.Click += new System.EventHandler(this.zmena_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(204, 72);
+            this.Controls.Add(this.zmena);
+            this.Controls.Add(this.vystup);
+            this.Controls.Add(this.vstup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Úkol 3 pole C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox vstup;
+        private System.Windows.Forms.TextBox vystup;
+        private System.Windows.Forms.Button zmena;
     }
 }
 
--- Ukol3_pole_Csharp/Form1.cs
+++ Ukol3_pole_Csharp/Form1.cs
@@ -16,5 +16,64 @@ namespace Ukol3_pole_Csharp
         {
             InitializeComponent();
         }
+
+        private void zmena_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                vystup.Clear();
+
+                char[] pole = vstup.Text.ToCharArray();
+                char max = '0';
+                
+
+                int pocet = pole.Length - 1;
+                int pozice_max = 0;
+                int i = 0;
+                int i2 = 0;
+                int y = 0;
+
+                if (pole[pocet] >= '0' && pole[pocet] <= '9')
+                {
+                    while (i <= pocet)
+                    {
+                        if (pole[i] >= '0' && pole[i] <= '9')
+                        {
+                            pole[i] = 'x';
+                        }
+
+                        i++;
+                    }
+                }
+                else
+                {
+                    while (i2 <= pocet)
+                    {
+                        if (pole[i2] > max)
+                        {
+                            max = pole[i2];
+                            pozice_max = i2;
+                        }
+
+                        i2++;
+                    }
+
+                    char posledni = pole[pocet];
+                    pole[pocet] = max;
+                    pole[pozice_max] = posledni;
+                    MessageBox.Show(Convert.ToString(max));
+                }
+
+                while (y <= pocet)
+                {
+                    vystup.AppendText(Convert.ToString(pole[y]));
+                    y++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Prvně zadej nějaké znaky!", "Error");
+            }
+        }
     }
 }
